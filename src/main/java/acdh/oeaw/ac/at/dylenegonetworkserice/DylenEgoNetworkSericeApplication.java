b933428fc30605140bf1c92f9f1572c134a947c4
Seungbin Yim <liam21c@gmail.com>
2020-07-07 16:20:48 +0200

    Initial Commit

---
+++ src/main/java/acdh/oeaw/ac/at/dylenegonetworkserice/DylenEgoNetworkSericeApplication.java
@@ -0,0 +1,13 @@
+package acdh.oeaw.ac.at.dylenegonetworkserice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DylenEgoNetworkSericeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DylenEgoNetworkSericeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/acdh/oeaw/ac/at/dylenegonetworkserice/DylenEgoNetworkSericeApplicationTests.java
@@ -0,0 +1,13 @@
+package acdh.oeaw.ac.at.dylenegonetworkserice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DylenEgoNetworkSericeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
