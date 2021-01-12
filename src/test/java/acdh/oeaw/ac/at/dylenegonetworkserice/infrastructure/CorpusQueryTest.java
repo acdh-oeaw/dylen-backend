6b8c57d4de94c586ff4c12794b60090e3e4be998
Seungbin Yim <liam21c@gmail.com>
2021-01-12 11:46:54 +0100

    Remove umlaute

--- src/test/java/acdh/oeaw/ac/at/dylenegonetworkserice/infrastructure/CorpusQueryTest.java
+++ src/test/java/acdh/oeaw/ac/at/dylenegonetworkserice/infrastructure/CorpusQueryTest.java
@@ -1,18 +1,17 @@
 package acdh.oeaw.ac.at.dylenegonetworkserice.infrastructure;
 
-import acdh.oeaw.ac.at.dylenegonetworkserice.service.EgoNetworkService;
 import acdh.oeaw.ac.at.dylenegonetworkserice.service.CorpusService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.testcontainers.shaded.com.google.common.collect.ImmutableList;
 
 import static acdh.oeaw.ac.at.dylenegonetworkserice.TestFixture.CORPUS_1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class CorpusQueryTest {
     @Mock
     CorpusService corpusService;
--- src/test/java/acdh/oeaw/ac/at/dylenegonetworkserice/infrastructure/NetworkQueryIT.java
+++ src/test/java/acdh/oeaw/ac/at/dylenegonetworkserice/infrastructure/NetworkQueryIT.java
@@ -1,22 +1,13 @@
 package acdh.oeaw.ac.at.dylenegonetworkserice.infrastructure;
 
-import acdh.oeaw.ac.at.dylenegonetworkserice.AppConfig;
 import acdh.oeaw.ac.at.dylenegonetworkserice.TestFixture;
-import acdh.oeaw.ac.at.dylenegonetworkserice.domain.TargetWord;
 import acdh.oeaw.ac.at.dylenegonetworkserice.persistence.repository.TargetWordRepository;
 import acdh.oeaw.ac.at.dylenegonetworkserice.service.NetworkService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 import java.util.Optional;
